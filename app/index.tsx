e47cb73e1e8a9748994a306e1028c2414fc9df12
henry komatsu <komatsuhenry@gmail.com>
2026-02-02 17:51:33 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>sadsadasdadsada screen.</Text>
+      <View>
+        <Text>oi</Text>
+      </View>
+    </View>
+  );
+}
